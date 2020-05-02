0258eb1b3976cecb462aa45ecfc33a30d41f3395
Brook Beyene <brookbeyene37@gmail.com>
2020-05-02 03:20:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,6 +16,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  result = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  return result
 end
 
 def aoh_lookup(aoh, row, key)
